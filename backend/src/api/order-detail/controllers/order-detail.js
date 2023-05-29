a7b4e34cb42a798671243e3851b431388804b5d8
Zignuts Technolab <ztlab153@Zignutss-Mac-mini.local>
2023-05-29 14:50:28 +0530

    Add velidation for menu-items

--- backend/src/api/order-detail/controllers/order-detail.js
+++ backend/src/api/order-detail/controllers/order-detail.js
@@ -1,108 +1,128 @@
-'use strict';
+"use strict";
 
 /**
  * order-detail controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order-detail.order-detail',
-({ strapi }) => ({
-  // Create order details
-  async create(ctx) {
-    console.log(ctx.request.body.data);
-    // Get request body data
-    const { quantity, order, menuItem } = ctx.request.body.data;
+module.exports = createCoreController(
+  "api::order-detail.order-detail",
+  ({ strapi }) => ({
+    // Create order details
+    async create(ctx) {
+      console.log(ctx.request.body.data);
+      // Get request body data
+      const { quantity, order, menuItem } = ctx.request.body.data;
 
-    // check if order exists or not
-    const existedOrder = await strapi.db.query("api::order.order").findOne({
-      where: { id: order },
-      populate: true,
-    });
-    if (!existedOrder || existedOrder.status === "Complete") {
-      return (ctx.status = 400), (ctx.body = "Order not found");
-    }
-    // Check if menu item exists or not
-    const existedMenuItem = await strapi.entityService.findOne(
-      "api::menu-item.menu-item",
-      menuItem,
-      {}
-    );
-    if (!existedMenuItem) {
-      return (ctx.status = 400), (ctx.body = "Menu Item not found");
-    }
+      // check if order exists or not
+      const existedOrder = await strapi.db.query("api::order.order").findOne({
+        where: { id: order },
+        populate: true,
+      });
+      if (!existedOrder || existedOrder.status === "Complete") {
+        return (ctx.status = 400), (ctx.body = "Order not found");
+      }
+      // Check if menu item exists or not
+      const existedMenuItem = (
+        await strapi.db
+          .query("api::menu-item.menu-item")
+          .findMany({
+            where: {
+              category: {
+                id: {
+                  $in: (
+                    await strapi.db
+                      .query("api::restaurant.restaurant")
+                      .findOne({
+                        where: { id: existedOrder.restaurant.id },
+                        populate: true,
+                      })
+                  ).categories.map((cat) => cat.id),
+                },
+              },
+            },
+            select: ["id"],
+          })
+          .then((item) => {
+            return item.map((item) => item.id);
+          })
+      ).includes(parseInt(menuItem));
+      if (!existedMenuItem) {
+        return (ctx.status = 400), (ctx.body = "Menu Item not found");
+      }
 
-    // count total price of current menu item
-    const total = existedMenuItem.price * quantity;
-    // Create new order-detail
-    const entry = await strapi.db
-      .query("api::order-detail.order-detail")
-      .create({
+      // count total price of current menu item
+      const total = existedMenuItem.price * quantity;
+      // Create new order-detail
+      const entry = await strapi.db
+        .query("api::order-detail.order-detail")
+        .create({
+          data: {
+            quantity: quantity,
+            order: order,
+            menuItem: menuItem,
+            total,
+            price: existedMenuItem.price,
+          },
+        })
+        .then((res) => {
+          return ctx.send(res);
+        })
+        .catch((error) => {
+          console.log(error);
+          return (ctx.status = 400);
+        });
+
+      // Update order according to created order details
+      await strapi.db.query("api::order.order").update({
+        where: { id: order },
         data: {
-          quantity: quantity,
-          order: order,
-          menuItem: menuItem,
-          total,
-          price: existedMenuItem.price,
+          totalQuantity: existedOrder.totalQuantity + quantity,
+          totalAmount: existedOrder.totalAmount + total,
         },
-      })
-      .then((res) => {
-        return ctx.send(res);
-      })
-      .catch((error) => {
-        console.log(error);
-        return (ctx.status = 400);
       });
+      return entry;
+    },
 
-    // Update order according to created order details
-    await strapi.db.query("api::order.order").update({
-      where: { id: order },
-      data: {
-        totalQuantity: existedOrder.totalQuantity + quantity,
-        totalAmount: existedOrder.totalAmount + total,
-      },
-    });
-    return entry;
-  },
-
-  // Delete an order detail from order
-  async delete(ctx) {
-    // Get request body data
-    const id = ctx.request.params.id;
-    console.log(id);
-    // Check if order detail exists or not
-    const existedOrderDetail = await strapi.db
-      .query("api::order-detail.order-detail")
-      .findOne({ where: { id: id }, populate: true });
-    if (!existedOrderDetail) {
-      return (ctx.status = 400), (ctx.body = "Order Detail not found");
-    }
-    console.log(existedOrderDetail);
-    await strapi.db.query("api::order.order").update({
-      where: { id: existedOrderDetail.order.id },
-      data: {
-        totalQuantity:
-          existedOrderDetail.order.totalQuantity -
-          existedOrderDetail.quantity,
-        totalAmount:
-          existedOrderDetail.order.totalAmount -
-          existedOrderDetail.menuItem.price,
-      },
-    });
-    // Delete order-detail
-    const deletedOrderDetail = await strapi.db
-      .query("api::order-detail.order-detail")
-      .delete({
-        where: { id: existedOrderDetail.id },
-      })
-      .then((res) => {
-        return ctx.send(res);
-      })
-      .catch((error) => {
-        console.log(error);
-        return (ctx.status = 400);
+    // Delete an order detail from order
+    async delete(ctx) {
+      // Get request body data
+      const id = ctx.request.params.id;
+      console.log(id);
+      // Check if order detail exists or not
+      const existedOrderDetail = await strapi.db
+        .query("api::order-detail.order-detail")
+        .findOne({ where: { id: id }, populate: true });
+      if (!existedOrderDetail) {
+        return (ctx.status = 400), (ctx.body = "Order Detail not found");
+      }
+      console.log(existedOrderDetail);
+      await strapi.db.query("api::order.order").update({
+        where: { id: existedOrderDetail.order.id },
+        data: {
+          totalQuantity:
+            existedOrderDetail.order.totalQuantity -
+            existedOrderDetail.quantity,
+          totalAmount:
+            existedOrderDetail.order.totalAmount -
+            existedOrderDetail.menuItem.price,
+        },
       });
-    return deletedOrderDetail;
-  },
-})
+      // Delete order-detail
+      const deletedOrderDetail = await strapi.db
+        .query("api::order-detail.order-detail")
+        .delete({
+          where: { id: existedOrderDetail.id },
+        })
+        .then((res) => {
+          return ctx.send(res);
+        })
+        .catch((error) => {
+          console.log(error);
+          return (ctx.status = 400);
+        });
+      return deletedOrderDetail;
+    },
+  })
 );
