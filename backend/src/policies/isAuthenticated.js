8fff8586e762b34acd63ed72975fe7eb8cd06a80
Zignuts Technolab <ztlab140@Zignutss-Mac-mini.local>
2023-05-30 15:40:28 +0530

    Comments Done

--- backend/src/policies/isAuthenticated.js
+++ backend/src/policies/isAuthenticated.js
@@ -1,21 +1,24 @@
 const utils = require("@strapi/utils");
 const { PolicyError } = utils.errors;
 
-module.exports = async (policyContext, config, { strapi }) => {
+module.exports = async (policyContext, { strapi }) => {
   let { params, request } = policyContext;
 
+  // Fetch current User
   const fetchUser = async () => {
     return await strapi.db
       .query("plugin::users-permissions.user")
       .findOne({ where: { id: policyContext.state.user.id }, populate: true });
   };
 
+  // Fetch restaurant of given id
   const fetchRest = async (id) => {
     return await strapi.db
       .query("api::restaurant.restaurant")
       .findOne({ where: { id }, populate: true });
   };
 
+  // Fetch order of given id and param
   const fetchOrder = async (id, param) => {
     return await strapi.db
       .query("api::order.order")
@@ -30,6 +33,7 @@ module.exports = async (policyContext, config, { strapi }) => {
       });
   };
 
+  // Fetch customer with populated orders
   const fetchCustomer = async (id) => {
     return await strapi.db
       .query("api::customer.customer")
@@ -38,23 +42,31 @@ module.exports = async (policyContext, config, { strapi }) => {
 
   if (policyContext.state.user) {
     switch (policyContext.state.user.role.type) {
+      // if current user is authenticated - Super Admin then return true
       case "authenticated":
         return true;
         break;
+      // if current user is Owner, Manager or Worker
       case "restaurant_owner":
       case "restaurant_worker":
       case "restaurant_manager":
         {
+          // if path is orders or orders id
           switch (policyContext.state.route.path) {
             case "/orders":
             case "/orders/:id":
               {
+                // Fetch user
                 const user = await fetchUser();
+                // if user or user's restaurant not found then throw error
                 if (!user || !user.restaurant) {
                   throw new PolicyError("Data not found for you!");
                 }
+                // check for user id path
                 if (params.id) {
+                  // if order exists for given retaurant id return true else throw error
                   if (
+                    // fetch order for that restaurant id
                     (
                       await fetchOrder(user.restaurant.id, "restaurant")
                     ).includes(parseInt(params.id))
@@ -73,22 +85,28 @@ module.exports = async (policyContext, config, { strapi }) => {
               return true;
               break;
 
+            // if path is restaurant or restaurant id
             case "/restaurants":
             case "/restaurants/:id":
               {
+                // Fetch user
                 const user = await fetchUser();
+                // if user or user's restaurant not found then throw error
                 if (!user || !user.restaurant) {
                   throw new PolicyError("Data not found for you!");
                 }
+                // if user is not login in his restaurant then throw error
                 if ((params.id && parseInt(params.id)) !== user.restaurant.id) {
                   throw new PolicyError("Data not found!");
                 } else if (request.query.filters) {
+                  // if filter doesn't contains users restaurant id then throw error
                   if (
                     parseInt(request.query.filters.id) !== user.restaurant.id
                   ) {
                     throw new PolicyError("Data not found!");
                   }
                 } else {
+                  // filter restaurants with login restaurant user
                   request.query.filters = {
                     ...request.query.filters,
                     id: user.restaurant.id,
@@ -98,30 +116,38 @@ module.exports = async (policyContext, config, { strapi }) => {
               return true;
               break;
 
+            // if path is categories or categories id
             case "/categories":
             case "/categories/:id":
               {
+                // Fetch user
                 const user = await fetchUser();
+                // if user or user's restaurant not found then throw error
                 if (!user || !user.restaurant) {
                   throw new PolicyError("Data not found for you!");
                 }
-
+                // for categories id path
                 if (params.id) {
+                  // Fetch restaurant of current user
                   const rest = await fetchRest(user.restaurant.id);
+                  // get categories id of current user's restaurant
                   const inArray = rest.categories.map((category) => {
                     return category.id.toString();
                   });
+                  // if list not contains params category id then throw error
                   if (!inArray.includes(params.id)) {
                     throw new PolicyError("Data not found!");
                   }
                 } else {
                   if (
+                    // if method is POST and user's restaurant id is not category's restaurant id then return false
                     policyContext.state.route.method === "POST" &&
                     parseInt(request.body.data.restaurant) !==
                       user.restaurant.id
                   ) {
                     return false;
                   }
+                  // filter categories of current user's restaurant
                   request.query.filters = {
                     ...request.query.filters,
                     restaurant: user.restaurant.id,
@@ -131,9 +157,11 @@ module.exports = async (policyContext, config, { strapi }) => {
               return true;
               break;
 
+            // if path is menu-item or menu-item id
             case "/menu-items":
             case "/menu-items/:id":
               {
+                // for menu-item id path
                 if (params.id) {
                   if (
                     (
@@ -201,11 +229,13 @@ module.exports = async (policyContext, config, { strapi }) => {
     }
     return true;
   }
+  // if path is restaurant or restaurant id and method is GET
   if (
     (policyContext.state.route.path === "/restaurants" ||
       policyContext.state.route.path === "/restaurants/:id") &&
     policyContext.state.route.method === "GET"
   ) {
+    // can populate categories only
     if (request.query.populate) {
       request.query.populate = {
         customers: false,
@@ -216,11 +246,13 @@ module.exports = async (policyContext, config, { strapi }) => {
     }
     return true;
   }
+  // if path is categories or categories id and method is GET
   if (
     (policyContext.state.route.path === "/categories" ||
       policyContext.state.route.path === "/categories/:id") &&
     policyContext.state.route.method === "GET"
   ) {
+    // populate menu-items only
     if (request.query.populate) {
       request.query.populate = {
         restaurant: false,
@@ -229,14 +261,18 @@ module.exports = async (policyContext, config, { strapi }) => {
     }
     return true;
   }
+  // if path is orders or orders id and method is GET
   if (
     (policyContext.state.route.path === "/orders" &&
       policyContext.state.route.method === "GET") ||
     policyContext.state.route.path === "/orders/:id"
   ) {
+    // Check if customer token is provided or not
     if (request.header.token && request.header.token.id) {
+      // if path is orders id
       if (params.id) {
         if (
+          // fetch order
           (await fetchOrder(request.header.token.id, "customer")).includes(
             parseInt(params.id)
           )
@@ -246,6 +282,7 @@ module.exports = async (policyContext, config, { strapi }) => {
           throw new PolicyError("Data not found!");
         }
       } else {
+        // filter with customer id
         request.query.filters = {
           ...request.query.filters,
           customer: { id: request.header.token.id },
@@ -256,15 +293,18 @@ module.exports = async (policyContext, config, { strapi }) => {
     }
     return true;
   }
-
+  // if path is order-details or order-details id and method is GET
   if (
     (policyContext.state.route.path === "/order-details" &&
       policyContext.state.route.method === "GET") ||
     policyContext.state.route.path === "/order-details/:id"
   ) {
+    // if there exists customer token
     if (request.header.token && request.header.token.id) {
+      // if order-details id exists
       if (params.id) {
         if (
+          // find order-details
           (
             await strapi.db
               .query("api::order-detail.order-detail")
@@ -306,12 +346,15 @@ module.exports = async (policyContext, config, { strapi }) => {
     }
     return true;
   }
+  // if path is order-details and method is POST
   if (
     policyContext.state.route.path === "/order-details" &&
     policyContext.state.route.method === "POST"
   ) {
+    // Check if customer token exists
     if (request.header.token && request.header.token.id) {
       if (
+        // Fetch order
         (await fetchOrder(request.header.token.id, "customer")).includes(
           parseInt(request.body.data.order)
         )
@@ -325,18 +368,27 @@ module.exports = async (policyContext, config, { strapi }) => {
     }
     return true;
   }
+  // if path is customers r customers id and method is GET
   if (
     (policyContext.state.route.path === "/customers" &&
       policyContext.state.route.method === "GET") ||
     policyContext.state.route.path === "/customers/:id"
   ) {
+    // if customer token exists
     if (request.header.token && request.header.token.id) {
       if (params.id && parseInt(params.id) !== request.header.token.id) {
         return false;
       } else {
-        if (request.query.populate && (parseInt(params.id) !== request.header.token.id)) {
+        if (
+          request.query.populate &&
+          parseInt(params.id) !== request.header.token.id
+        ) {
           request.query.populate = {};
         }
+        request.query.filters = {
+          ...request.query.filters,
+          id: request.header.token.id,
+        };
         return true;
       }
     } else {
--- backend/src/policies/userPermission.js
+++ backend/src/policies/userPermission.js
@@ -3,6 +3,7 @@ const { PolicyError } = utils.errors;
 module.exports = async (policyContext, config, { strapi }) => {
   let { params, request } = policyContext;
 
+  // Fetch User
   const fetchUser = async (id) => {
     return await strapi.db
       .query("plugin::users-permissions.user")
@@ -12,6 +13,8 @@ module.exports = async (policyContext, config, { strapi }) => {
   let manager;
   let worker;
   let owner;
+
+  // Fetch Role and get role id of all roles
   const fetchRole = async () => {
     const roles = await strapi.db
       .query("plugin::users-permissions.role")
@@ -30,17 +33,22 @@ module.exports = async (policyContext, config, { strapi }) => {
     return roles;
   };
 
+  // Check policies for different roles
   if (policyContext.state.user) {
     switch (policyContext.state.user.role.type) {
+      // Check policy for authenticated - Super Admin
       case "authenticated":
+        // If path contains user id and method is Update , Get or Delete
         if (
           policyContext.state.route.path === "/users/:id" &&
           (policyContext.state.route.method === "DELETE" ||
             policyContext.state.route.method === "PUT" ||
             policyContext.state.route.method === "GET")
         ) {
+          // Fetch role
           await fetchRole();
 
+          // Stop populate for GET method when login user is not owner, manager or worker
           if (
             request.body.role !== owner &&
             request.body.role !== manager &&
@@ -55,11 +63,15 @@ module.exports = async (policyContext, config, { strapi }) => {
             return false;
           }
         }
+
+        //If path contains users and method is POST
         if (
           policyContext.state.route.path === "/users" &&
           policyContext.state.route.method === "POST"
         ) {
+          // Fetch role
           await fetchRole();
+          // if role is not owner manager or worker then he can't create user
           if (
             request.body.role !== owner &&
             request.body.role !== manager &&
@@ -68,10 +80,13 @@ module.exports = async (policyContext, config, { strapi }) => {
             return false;
           }
         }
+
+        // If path contains users and method is GET
         if (
           policyContext.state.route.path === "/users" &&
           policyContext.state.route.method === "GET"
         ) {
+          // Fetch role
           const roles = await fetchRole();
           const reqRoles = [];
           roles.forEach((role) => {
@@ -92,23 +107,30 @@ module.exports = async (policyContext, config, { strapi }) => {
         }
         return true;
         break;
+
+      // Check policies for Restaurant Owner
       case "restaurant_owner":
+        // if path is users id and method is Update, Get and Delete
         if (
           policyContext.state.route.path === "/users/:id" &&
           (policyContext.state.route.method === "DELETE" ||
             policyContext.state.route.method === "PUT" ||
             policyContext.state.route.method === "GET")
         ) {
+          // Fetch user and fetch current login user
           const user = await fetchUser(params.id);
           const self = await fetchUser(policyContext.state.user.id);
+          // is user and his restaurant not found then throw error
           if (!user || !user.restaurant || !self || !self.restaurant) {
             throw new PolicyError("Data not found for you!");
           }
+          // if restaurant id doesn't match and role is not owner or manager then
           if (
             user.restaurant.id !== self.restaurant.id &&
             user.role.type !== "restaurant_manager" &&
             user.role.type !== "restaurant_worker"
           ) {
+            // if method is GET then stop populate
             if (policyContext.state.route.method === "GET") {
               if (request.query.populate) {
                 request.query.populate = {};
@@ -118,23 +140,30 @@ module.exports = async (policyContext, config, { strapi }) => {
             return false;
           }
         }
+        // if path is users and method is POST
         if (
           policyContext.state.route.path === "/users" &&
           policyContext.state.route.method === "POST"
         ) {
+          // Fetch role
           await fetchRole();
+          // stop this route if role is not manager or worker
           if (request.body.role !== manager && request.body.role !== worker) {
             return false;
           }
         }
+        //if path is users and method is GET
         if (
           policyContext.state.route.path === "/users" &&
           policyContext.state.route.method === "GET"
         ) {
+          // Fetch user
           const user = await fetchUser(policyContext.state.user.id);
+          // is user or user's restaurant not found then throw error
           if (!user || !user.restaurant) {
             throw new PolicyError("Data not found for you!");
           }
+          //Fetch roles
           const roles = await fetchRole();
           const reqRoles = [];
           roles.forEach((role) => {
@@ -155,7 +184,10 @@ module.exports = async (policyContext, config, { strapi }) => {
         }
         return true;
         break;
+
+      // Check policies for Restaurant Worker
       case "restaurant_worker":
+        // if path is not user/me and wants to populate then stop populate
         if (
           request.query.populate &&
           policyContext.state.route.path !== "/users/me"
@@ -164,23 +196,30 @@ module.exports = async (policyContext, config, { strapi }) => {
         }
         return true;
         break;
+
+      // Check policies for Restaurant Manager
       case "restaurant_manager":
+        //if path is users id and method is UPDATE, DELETE and GET
         if (
           policyContext.state.route.path === "/users/:id" &&
           (policyContext.state.route.method === "DELETE" ||
             policyContext.state.route.method === "PUT" ||
             policyContext.state.route.method === "GET")
         ) {
+          // Fetch user & fetch login user
           const user = await fetchUser(params.id);
           const self = await fetchUser(policyContext.state.user.id);
+          // if user is not found or user's restaturant is not found then throw error
           if (!user || !user.restaurant || !self || !self.restaurant) {
             throw new PolicyError("Data not found for you!");
           }
+          // if current login user's restaurant id is not equals to params user's restaurant and user is not manager or worker
           if (
             user.restaurant.id !== self.restaurant.id &&
             (user.role.type !== "restaurant_manager" ||
               user.role.type !== "restaurant_worker")
           ) {
+            // then stop populate for GET method
             if (policyContext.state.route.method === "GET") {
               if (request.query.populate) {
                 request.query.populate = {};
@@ -190,23 +229,30 @@ module.exports = async (policyContext, config, { strapi }) => {
             return false;
           }
         }
+        // if path is users and method is POST
         if (
           policyContext.state.route.path === "/users" &&
           policyContext.state.route.method === "POST"
         ) {
+          // Fetch role
           await fetchRole();
+          // return false if role is not worker
           if (request.body.role !== worker) {
             return false;
           }
         }
+        // if path is users and method is GET
         if (
           policyContext.state.route.path === "/users" &&
           policyContext.state.route.method === "GET"
         ) {
+          //Fetch current login user
           const user = await fetchUser(policyContext.state.user.id);
+          // if user not found or user's restaurant not found
           if (!user || !user.restaurant) {
             throw new PolicyError("Data not found for you!");
           }
+          // Fetch roles
           const roles = await fetchRole();
           const reqRoles = [];
           roles.forEach((role) => {
@@ -230,21 +276,13 @@ module.exports = async (policyContext, config, { strapi }) => {
         break;
     }
   }
-  if (
-    policyContext.state.route.path === "/users" &&
-    policyContext.state.route.method === "POST"
-  ) {
-    await fetchRole();
-    if (request.body.role !== customer) {
-      throw new PolicyError("You can not create this!");
-    }
-    return true;
-  }
+  // if path is users or users id and method is GET
   if (
     (policyContext.state.route.path === "/users" ||
       policyContext.state.route.path === "/users/:id") &&
     policyContext.state.route.method === "GET"
   ) {
+    // populate restaurants only
     if (request.query.populate) {
       request.query.populate = {
         role: false,
